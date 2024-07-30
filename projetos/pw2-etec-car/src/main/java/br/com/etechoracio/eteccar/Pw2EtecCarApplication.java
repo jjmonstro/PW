392d8d5c296a758d6daa38465dfac11448820ad9
jjmonstro <jpcorreia1211@gmail.com>
2024-07-30 08:57:17 -0300

    new project

---
+++ projetos/pw2-etec-car/src/main/java/br/com/etechoracio/eteccar/Pw2EtecCarApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.eteccar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw2EtecCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw2EtecCarApplication.class, args);
+	}
+
+}
